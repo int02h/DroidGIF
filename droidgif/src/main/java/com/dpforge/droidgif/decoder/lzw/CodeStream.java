5e13a18ae52bbf01fd181b0b2b9708d3eef473cc
Daniil Popov <int02h@gmail.com>
2016-01-19 00:06:21 +0300

    lzw implementation

---
+++ droidgif/src/main/java/com/dpforge/droidgif/decoder/lzw/CodeStream.java
@@ -0,0 +1,44 @@
+package com.dpforge.droidgif.decoder.lzw;
+
+class CodeStream {
+	private byte[] mData;
+	private int mCodeSize;
+	private byte[] mBits;
+	private int mIndex;
+
+	CodeStream(byte[] data, int codeSize) {
+		mData = data;
+		mCodeSize = codeSize;
+
+		mBits = new byte[mData.length*8];
+		for (int i = 0; i < mData.length; ++i) {
+			for (int j = 0; j < 8; ++j) {
+				byte bit = getBit(mData[i], j);
+				mBits[i*8 + j] = bit;
+			}
+		}
+		mIndex = 0;
+	}
+
+	void setCodeSize(int size) {
+		mCodeSize = size;
+	}
+
+	boolean hasData() {
+		return mIndex + mCodeSize <= mBits.length;
+	}
+
+	int nextValue() {
+		int res = 0;
+		for (int pos = 0; pos < mCodeSize; ++mIndex, ++pos) {
+			res += (mBits[mIndex] << pos);
+		}
+		return res;
+	}
+
+	private static byte getBit(int value, int index) {
+		int mask = (1 << index);
+		return (byte) (((value & mask) == mask) ? 1 : 0);
+	}
+}
+
