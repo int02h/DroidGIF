f48a820bc67eaa04e9996d456508a878bbb681a6
Daniil Popov <int02h@gmail.com>
2016-01-18 22:46:02 +0300

    stub for comment extension

---
+++ droidgif/src/main/java/com/dpforge/droidgif/datastream/CommentExtension.java
@@ -0,0 +1,11 @@
+package com.dpforge.droidgif.datastream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CommentExtension extends ExtensionBlock {
+	@Override
+	void read(final InputStream is) throws IOException, InvalidDataStreamException {
+		// TODO
+	}
+}
