ce6103fb593f048980ee3045d6cbee6e89e28436
Daniil Popov <int02h@gmail.com>
2016-01-22 14:24:36 +0300

    test app

--- app/src/main/java/com/dpforge/droidgiftestapp/MainActivity.java
+++ app/src/main/java/com/dpforge/droidgiftestapp/MainActivity.java
@@ -3,11 +3,15 @@ package com.dpforge.droidgiftestapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dpforge.droidgif.GIFImageView;
+
 public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		final GIFImageView gif = new GIFImageView(this);
+		gif.setImageResource(R.raw.earthquake);
+		setContentView(gif);
 	}
 }
