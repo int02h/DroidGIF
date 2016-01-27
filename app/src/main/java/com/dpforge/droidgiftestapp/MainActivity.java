be982b19d5af152f872d523a11e390065dc068bd
Daniil Popov <int02h@gmail.com>
2016-01-27 22:48:35 +0300

    test app

--- app/src/main/java/com/dpforge/droidgiftestapp/MainActivity.java
+++ app/src/main/java/com/dpforge/droidgiftestapp/MainActivity.java
@@ -2,16 +2,28 @@ package com.dpforge.droidgiftestapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.dpforge.droidgif.GIFImageView;
 
 public class MainActivity extends AppCompatActivity {
+	private GIFImageView[] mGifViews;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		final GIFImageView gif = new GIFImageView(this);
-		gif.setImageResource(R.raw.big_with_comment);
-		setContentView(gif);
+		setContentView(R.layout.activity_main);
+
+		mGifViews = new GIFImageView[]{
+				(GIFImageView) findViewById(R.id.gif1),
+				(GIFImageView) findViewById(R.id.gif2),
+				(GIFImageView) findViewById(R.id.gif3)
+		};
+	}
+
+	public void onLoadGifsClick(View view) {
+		mGifViews[0].setImageResource(R.raw.big_with_comment);
+		mGifViews[1].setImageResource(R.raw.rotating_earth);
+		mGifViews[2].setImageResource(R.raw.earthquake);
 	}
 }
