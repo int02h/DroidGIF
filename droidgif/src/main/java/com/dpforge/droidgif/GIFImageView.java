55c701175b3d71a6fc33a59ac4f395e2dc1a468f
Daniil Popov <int02h@gmail.com>
2016-01-27 22:48:26 +0300

    onMeasure impl

--- droidgif/src/main/java/com/dpforge/droidgif/GIFImageView.java
+++ droidgif/src/main/java/com/dpforge/droidgif/GIFImageView.java
@@ -2,6 +2,7 @@ package com.dpforge.droidgif;
 
 import android.content.Context;
 import android.support.annotation.RawRes;
+import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -15,11 +16,53 @@ import java.io.InputStream;
 
 public class GIFImageView extends SurfaceView {
 	private RendererThread mRendererThread;
+	private GIFImage mImage;
 
 	public GIFImageView(final Context context) {
 		super(context);
-		getHolder().addCallback(new SurfaceHolderCallback());
-		mRendererThread = new RendererThread(getHolder());
+		init();
+	}
+
+	public GIFImageView(final Context context, final AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	@Override
+	protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
+		int desiredWidth, desiredHeight;
+		if (mImage != null) {
+			desiredWidth = mImage.width();
+			desiredHeight = mImage.height();
+		} else {
+			desiredWidth = desiredHeight = 0;
+		}
+
+		int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+		int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+		int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+		int width;
+		int height;
+
+		if (widthMode == MeasureSpec.EXACTLY) {
+			width = widthSize;
+		} else if (widthMode == MeasureSpec.AT_MOST) {
+			width = Math.min(desiredWidth, widthSize);
+		} else {
+			width = desiredWidth;
+		}
+
+		if (heightMode == MeasureSpec.EXACTLY) {
+			height = heightSize;
+		} else if (heightMode == MeasureSpec.AT_MOST) {
+			height = Math.min(desiredHeight, heightSize);
+		} else {
+			height = desiredHeight;
+		}
+
+		setMeasuredDimension(width, height);
 	}
 
 	public void setImageResource(@RawRes int resId) {
@@ -27,12 +70,19 @@ public class GIFImageView extends SurfaceView {
 		final BufferedInputStream bis = new BufferedInputStream(is);
 		try {
 			final GIFDecoder decoder = new GIFDecoder(bis);
-			final GIFImage image = decoder.decode();
+			mImage = decoder.decode();
 			bis.close();
-			mRendererThread.setImage(image);
+			mRendererThread.setImage(mImage);
+			requestLayout();
+			invalidate();
 		} catch (IOException | DecoderException  e) {
 			e.printStackTrace();
 		}
+	}
+
+	private void init() {
+		getHolder().addCallback(new SurfaceHolderCallback());
+		mRendererThread = new RendererThread(getHolder());
 	}
 
 	private class SurfaceHolderCallback implements SurfaceHolder.Callback {
